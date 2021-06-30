a6743220d9ab9507dc177580052c1d138da886df
mario-mitchell <35703472+mario-mitchell@users.noreply.github.com>
2021-06-29 20:37:15 -0700

    Update

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -1,6 +1,5 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
+    
     const password = req.query.password || req.body.password
 
     let your_response;
