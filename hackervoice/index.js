cfe939e4edf1a76340628a43718528ff9eb7f9b9
mario-mitchell <35703472+mario-mitchell@users.noreply.github.com>
2021-06-29 20:53:41 -0700

    update

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -11,7 +11,7 @@ module.exports = async function (context, req) {
     }
    
     context.res = {
-        // status: 200, /* Defaults to 200 */
+        // status: 200,/* Defaults to 200 */
         body: your_response
     } 
 }
